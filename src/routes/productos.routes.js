04cb8bd97ee109bb89a2ef9f62c80eaa94b767b4
Tulio <22460285@colima.tecnm.mx>
2026-02-11 14:52:55 -0600

    Creacion de enpoints get y post para productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,4 +7,31 @@ router.get('/productos', async (req, res)=>{
     const productos = await prisma.producto.findMany();
     res.json(productos);
 })
+
+router.get('/productos/:id', async (req, res)=>{
+    const productoEncontrado     = await prisma.producto.findFirst({
+        where:{
+            id: parseInt(req.params.id)
+        }
+    });
+    res.json(productoEncontrado);
+})
+router.post('/productos', async (req, res)=>{
+    try{
+        const nuevoProducto = await prisma.producto.create({
+            data: req.body
+        });
+        res.status(201).json({
+            "msg": "Se agregó el producto exitosamente"
+        });  
+    }catch(error){  
+        res.status(500).json({
+            "error": error
+        });  
+    }
+
+
+
+})
+
 export default router;
